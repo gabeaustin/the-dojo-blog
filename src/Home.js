095b2257e49b3f697bc00728f6d1b6927964f288
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-02-26 08:20:49 -0600

    added click event for components

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,20 @@
 const Home = () => {
+
+    const handleClick = (e) => {
+        console.log("Hello Ace", e);
+    }
+
+    const handleClickAgain = (name, e) => {
+        console.log(`Hello ${name}.`, e.target)
+    }
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click Me</button>
+            <button onClick={(e) => {
+                handleClickAgain("Mario", e);
+            }}>Click me again</button>
         </div>
     );
 
